87fa163b1e4e8605848986c78a6b7bf5c8d4cfdc
dineshchhabra <dinesh.chha11@gmail.com>
2015-07-31 09:49:28 +0530

    RummyHand Sugnature added

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,16 @@
 
-public class Card {
+class Card {
+	int cardId;
 
+	public boolean isSame(Card card){
+ 		return true;  
+	}
+	
+	public String getRank(){
+		return "1";
+	}
+	
+    	public String getSuit() {
+		return "1";
+	}
 }
--- src/RummyHand.java
+++ src/RummyHand.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
 
 public class RummyHand {
+	
+	Card cardsinHand[];
+	
+	RummyHand() {
+		cardsinHand = new Card[13];
+	}
+	public Card[] generateInput() {
+		return new Card[13];
+	}
+
+	boolean isNatural(ArrayList<Card> cards ){
+		return true;	
+	}	
+	
+	boolean isImproper( ArrayList<Card> cards) {
+		return true;
+	}
+	
+	boolean isCanasta( ArrayList<Card> cards ){
+		return true;
+	}
+	
+	boolean isSet( ArrayList<Card> cards ){
+		return true;
+	}
+	
+	public int noOfCardsForDeclaration( Card[] cards) {
+		return 0;
+	}
+
+	ArrayList<ArrayList<Card>> getAllValidSequence(Card [] cards) {
+		return new  ArrayList<ArrayList<Card>>();
+	}
+
+
+	public static void main(String[] args){
+    	
+		RummyHand hand = new RummyHand();
+		
+		//hand.noOfCardsForDeclaration();
+	}	
 
 }
